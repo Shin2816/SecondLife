5ca15997ea6e606e7ef74c6cb589942b3f064e10
th9519 <th9519@gmail.com>
2023-10-04 16:28:32 +0900

    왜 오류

--- src/main/java/com/green/SecondLife/community/service/CommunityServiceImpl.java
+++ src/main/java/com/green/SecondLife/community/service/CommunityServiceImpl.java
@@ -1,4 +1,19 @@
 package com.green.SecondLife.community.service;
 
-public class CommunityServiceImpl {
+import com.green.SecondLife.community.vo.BoardFreeListVO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.mybatis.spring.SqlSessionTemplate;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommunityServiceImpl implements CommunityService{
+    private final sqlSessionTemplate sqlSession;
+
+    @Override
+    public List<BoardFreeListVO> selectFreeBoardList(BoardFreeListVO boardFreeListVO) {
+        return null;
+    }
 }
