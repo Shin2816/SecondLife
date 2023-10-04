2f52d2958d85ef0d6333d1a61ddaefe48dfe3a90
hyunbyuck <shings0310@gmail.com>
2023-10-04 17:03:40 +0900

    hyunbyuck

--- src/main/java/com/green/SecondLife/instructor/service/InstructorService.java
+++ src/main/java/com/green/SecondLife/instructor/service/InstructorService.java
@@ -3,9 +3,11 @@ package com.green.SecondLife.instructor.service;
 import com.green.SecondLife.instructor.vo.InstructorVO;
 import com.green.SecondLife.instructor.vo.SubjectVO;
 
+import java.util.List;
+
 public interface InstructorService {
     //강사 등록화면에서 강좌 과목 카테고리 조회하는 기능
-    public SubjectVO selectSubject();
+    public List<SubjectVO> selectSubjectList();
     //강사 등록하는 기능
     public void insertInstructor(InstructorVO instructorVO);
 }
