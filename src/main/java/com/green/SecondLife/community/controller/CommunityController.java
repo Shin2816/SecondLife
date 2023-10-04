dfaa9a82834e19646e0fc03d0614e9e72551bd5e
th9519 <th9519@gmail.com>
2023-10-04 17:45:36 +0900

    test

--- src/main/java/com/green/SecondLife/community/controller/CommunityController.java
+++ src/main/java/com/green/SecondLife/community/controller/CommunityController.java
@@ -1,8 +1,13 @@
 package com.green.SecondLife.community.controller;
 
 import com.green.SecondLife.community.service.CommunityService;
+import com.green.SecondLife.community.vo.BoardFreeListVO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ListableBeanFactoryExtensionsKt;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+//import java.util.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -10,6 +15,10 @@ public class CommunityController {
     private final CommunityService communityService;
 
     //게시판 출력
+    @GetMapping("/freeBoardList")
+    public String freeBoardList(Model model){
 
+        return "";
+    }
 
 }
