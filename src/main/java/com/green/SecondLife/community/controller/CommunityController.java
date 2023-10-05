19915336980aec8dbce9afe2335ae0709cadbd5f
th9519 <th9519@gmail.com>
2023-10-05 17:47:34 +0900

    10.05

--- src/main/java/com/green/SecondLife/community/controller/CommunityController.java
+++ src/main/java/com/green/SecondLife/community/controller/CommunityController.java
@@ -2,14 +2,18 @@ package com.green.SecondLife.community.controller;
 
 import com.green.SecondLife.community.service.CommunityService;
 import com.green.SecondLife.community.vo.BoardFreeListVO;
+import com.green.SecondLife.member.vo.MemberVO;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.ListableBeanFactoryExtensionsKt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-//import java.util.*;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -17,9 +21,11 @@ public class CommunityController {
     private final CommunityService communityService;
 
     //게시판 출력
-    @GetMapping("/freeBoardList")
-    public String freeBoardList(Model model, BoardFreeListVO boardFreeListVO){
-        model.addAttribute("freeBoardList", communityService.selectFreeBoardList(boardFreeListVO));
+    @RequestMapping("/freeBoardList")
+    public String freeBoardList(Model model){
+        List<BoardFreeListVO> freeBoardList = communityService.selectFreeBoardList();
+        model.addAttribute("freeBoardList", freeBoardList);
+
         return "board/free_board";
     }
 
@@ -27,5 +33,37 @@ public class CommunityController {
     @GetMapping("/regBoardForm")
     private String regBoardForm(){
         return "board/reg_board";
+    }
+    //글 등록 페이지에서 등록하기 누르면 글 등록 쿼리 실행
+    @PostMapping("/regBoard")
+    private String regBoard(BoardFreeListVO boardFreeListVO, HttpSession session){
+        MemberVO loginInfo = (MemberVO)session.getAttribute("loginInfo");
+        boardFreeListVO.setFreeBoardWriter(loginInfo.getMemberId());
+        communityService.insertFreeBoard(boardFreeListVO);
+        return "redirect:/freeBoardList";
+    }
+    //글 제목 클릭했을때 해당글의 상세페이지 이동
+    @GetMapping("/boardDetail")
+    private String boardDetail(int freeBoardNum, Model model){
+        model.addAttribute("board", communityService.selectFreeBoardDetail(freeBoardNum));
+        return "board/board_detail";
+    }
+    //글 상세페이지에서 삭제버튼 클릭하였을 때
+    @GetMapping("/deleteFreeBoard")
+    private String deleteFreeBoard(int freeBoardNum){
+        communityService.deleteFreeBoard(freeBoardNum);
+        return "redirect:/freeBoardList";
+    }
+    //글 상세페이지에서 수정 버튼을 클릭하였을때 수정페이지로 이동
+    @GetMapping("/updateFreeBoardForm")
+    private String updateFreeBoardForm(int freeBoardNum, Model model){
+        model.addAttribute("board", communityService.selectFreeBoardDetail(freeBoardNum));
+        return "board/update_board";
+    }
+    //
+    @PostMapping("/updateBoard")
+    private String updateBoard(BoardFreeListVO boardFreeListVO, int boardNum){
+        communityService.updateFreeBoard(boardFreeListVO);
+        return "redirect:/freeBoardList";
     }
 }
--- src/main/java/com/green/SecondLife/community/service/CommunityService.java
+++ src/main/java/com/green/SecondLife/community/service/CommunityService.java
@@ -5,5 +5,14 @@ import com.green.SecondLife.community.vo.BoardFreeListVO;
 import java.util.List;
 
 public interface CommunityService {
-    public List<BoardFreeListVO> selectFreeBoardList(BoardFreeListVO boardFreeListVO);
+    //자유게시판 목록 조회
+    public List<BoardFreeListVO> selectFreeBoardList();
+    //자유게시판 글 등록
+    public int insertFreeBoard(BoardFreeListVO boardFreeListVO);
+    //자유게시판 글 상세조회
+    public BoardFreeListVO selectFreeBoardDetail(int freeBoardNum);
+    //자유게시판 글 삭제
+    public int deleteFreeBoard(int freeBoardNum);
+    //자유게시판 글 수정
+    public int updateFreeBoard(BoardFreeListVO boardFreeListVO);
 }
