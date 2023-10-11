d21b5f6916ab0f9047106654e6270f4d473dc1f5
Shin2816 <rbghks2953@gmail.com>
2023-10-11 21:20:39 +0900

    231011 2120

--- src/main/java/com/green/SecondLife/interceptor/InterceptorConfig.java
+++ src/main/java/com/green/SecondLife/interceptor/InterceptorConfig.java
@@ -15,13 +15,13 @@ public class InterceptorConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         //게시판 페이지 정보 출력하는 인터셉터의 실행 설정
         registry.addInterceptor(boardInterceptor)
-                .addPathPatterns("/**")
+                .addPathPatterns("/","/main")
                 .excludePathPatterns("/**/**Fetch")
                 .excludePathPatterns("/images/**", "/js/**", "/css/**");
 
         //강좌 페이지 정보 출력하는 인터셉터의 실행 설정
         registry.addInterceptor(lectureServiceInterceptor)
-                .addPathPatterns("/**")
+                .addPathPatterns("/")
                 .excludePathPatterns("/**/**Fetch")
                 .excludePathPatterns("/images/**", "/js/**", "/css/**");
     }
