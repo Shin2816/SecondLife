cb404f85434b76b8615365bde08ce5c7ba6884ef
hyekim2635 <hyekim2635@gmail.com>
2023-10-26 16:02:27 +0900

    ã풀캘린더 화면 css

--- src/main/resources/static/js/admin/manage_rental.js
+++ src/main/resources/static/js/admin/manage_rental.js
@@ -2,7 +2,7 @@
 function rejectBtn(signCode){
     if(confirm('해당 신청건을 반려하시겠습니까?')){
         const reasonInput = document.querySelector('input[name=rejectReason]');
-        location.href= '/rental/updateStateReject?rentalSignCode='+signCode+'&rejectReason='+reasonInput.value;
+        location.href= '/rental/updateStateReject?rentalSignCode='+signCode+'&rejectReason='+ reasonInput.value;
         alert('반려가 완료되었습니다.');
     }
 }
