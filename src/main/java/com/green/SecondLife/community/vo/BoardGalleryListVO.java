4363789b935890757570595cce37ed9b4c100c99
th9519 <th9519@gmail.com>
2023-10-29 21:07:27 +0900

    10/29home

--- src/main/java/com/green/SecondLife/community/vo/BoardGalleryListVO.java
+++ src/main/java/com/green/SecondLife/community/vo/BoardGalleryListVO.java
@@ -2,6 +2,8 @@ package com.green.SecondLife.community.vo;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class BoardGalleryListVO extends PageVO{
     private int galBoardNum;
@@ -12,4 +14,6 @@ public class BoardGalleryListVO extends PageVO{
     private int galBoardReadCnt;
     private String searchType;
     private String searchValue;
+    private List<GalleryImgVO> galImgList;//board클래스는 img클래스를 여러개 가진다.
+    private List<BoardCommentListVO> galBoardComment;// 사진 게시판 클래스는 댓글 클래스를 여러개 갖는다.
 }
--- src/main/java/com/green/SecondLife/community/vo/GalleryImgVO.java
+++ src/main/java/com/green/SecondLife/community/vo/GalleryImgVO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class GalleryImgVO {
     private String galleryCode;
-    private String galOriginImgName; // 실제 파일 이름
-    private String galServerImgName; // 서버 파일 이름
+    private String galOriginFileName; // 실제 파일 이름
+    private String galAttachedFileName; // 서버 파일 이름
     private int galBoardNum; // BOARD_GALLERY_LIST와 외래키 체결되어짐
 }
