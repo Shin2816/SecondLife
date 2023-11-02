0d4fa238da9b95953b4018abb4c224d400b463cb
th9519 <th9519@gmail.com>
2023-11-02 23:11:57 +0900

    home

--- src/main/java/com/green/SecondLife/community/controller/AnnounceController.java
+++ src/main/java/com/green/SecondLife/community/controller/AnnounceController.java
@@ -1,10 +1,37 @@
 package com.green.SecondLife.community.controller;
 
+import com.green.SecondLife.community.service.AnnounceService;
+import com.green.SecondLife.community.vo.BoardAnnounceListVO;
+import com.green.SecondLife.community.vo.BoardQaListVO;
+import com.green.SecondLife.member.vo.SubMenuVO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Controller
 public class AnnounceController {
+    private final AnnounceService announceService;
 
+    //Q&A 게시판 출력
+    @RequestMapping("/anBoardList")
+    public String qaBoardList(Model model, BoardAnnounceListVO boardAnnounceListVO, SubMenuVO subMenuVO, Authentication authentication){
+        //페이지 정보 세팅
+        int totalDataCnt = announceService.selectBoardCnt(); //전체 게시글 갯수 조회해서
+        boardAnnounceListVO.setTotalDataCnt(totalDataCnt);//세터 호출해서 전체 게시글 갯수 전달
+        boardAnnounceListVO.setPageInfo();//변수값 설정한 메소드 호출(상속관계라 사용가능)
+
+        model.addAttribute("authentication", authentication);
+
+        //게시글 목록 조회
+        List<BoardAnnounceListVO> anBoardList = announceService.selectAnBoardList(boardAnnounceListVO);
+        model.addAttribute("qaBoardList", anBoardList);
+        return "board/announce/an_board";
+    }
 }
--- src/main/java/com/green/SecondLife/community/service/AnnounceServiceImpl.java
+++ src/main/java/com/green/SecondLife/community/service/AnnounceServiceImpl.java
@@ -1,10 +1,49 @@
 package com.green.SecondLife.community.service;
 
+import com.green.SecondLife.community.vo.BoardAnnounceListVO;
 import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AnnounceServiceImpl implements AnnounceService{
-
+    private final SqlSessionTemplate sqlsession;
+    //공지사항 조회
+    @Override
+    public List<BoardAnnounceListVO> selectAnBoardList(BoardAnnounceListVO boardAnnounceListVO) {
+        return sqlsession.selectList("anMapper.selectAnBoardList", boardAnnounceListVO);
+    }
+    //공지사항 등록
+    @Override
+    public int insertAnBoard(BoardAnnounceListVO boardAnnounceListVO) {
+        return sqlsession.insert("anMapper.insertAnBoard", boardAnnounceListVO);
+    }
+    //공지사항 상세정보 출력
+    @Override
+    public BoardAnnounceListVO selectAnBoardDetail(int anBoardNum) {
+        return sqlsession.selectOne("anMapper.selectAnBoardDetail", anBoardNum);
+    }
+    //조회수
+    @Override
+    public int updateAnBoardCnt(int anBoardNum) {
+        return sqlsession.update("anMapper.updateAnBoardCnt", anBoardNum);
+    }
+    //공지사항 삭제
+    @Override
+    public int deleteAnBoard(int anBoardNum) {
+        return sqlsession.delete("anMapper.deleteAnBoard", anBoardNum);
+    }
+    //공지사항 수정
+    @Override
+    public int updateAnBoard(BoardAnnounceListVO boardAnnounceListVO) {
+        return sqlsession.update("anMapper.updateAnBoard", boardAnnounceListVO);
+    }
+    //게시글 갯수 조회(페이징)
+    @Override
+    public int selectBoardCnt() {
+        return sqlsession.selectOne("anMapper.selectBoardCnt");
+    }
 }
