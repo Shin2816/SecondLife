11414a2ac697a56fbc4287dc1043dabf4e3f9cc3
hyunbyuck <shings0310@gmail.com>
2023-11-03 14:43:59 +0900

    security

--- src/main/resources/static/js/admin/admin_insert_lecture_form.js
+++ src/main/resources/static/js/admin/admin_insert_lecture_form.js
@@ -3,3 +3,19 @@
 function submitBtn(){
     document.querySelector('#formTag').submit();
 }
+
+function dateCompare(thisInput){
+    startDate = document.querySelector('.startDate-inputTag');
+    console.log(thisInput.value);
+    console.log(startDate.value);
+    if(startDate.value != '' && thisInput.value == startDate.value){
+        alert('시작일과 종강일이 같을 수 없습니다');
+        thisInput.value = '';
+    } else if (startDate.value != '' && thisInput.value < startDate.value){
+        alert('종강일이 시작일보다 더 빠를 수 없습니다')
+        thisInput.value = '';
+    } else if (startDate.value == ''){
+        alert('시작일을 먼저 입력해주세요.')
+        thisInput.value = '';
+    }
+}
